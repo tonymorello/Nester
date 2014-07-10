95cbc52f4addd316d4eeb4c621736926c9abb608
Tony <tony@morellowebdesign.com>
2014-07-10 17:39:42 -0400

    stable

--- jquery.nester.js
+++ jquery.nester.js
@@ -1,103 +1,277 @@
 (function( $ ){
 	
-	$.fn.nester = function(options) {
+	var runNester = function(options){
 		
-		this.each(function(e){
-			nester(this, options);
-		});
+		var defaults = {
+			maxLevels : 4,
+			onStart : function(){},
+			onDrag : function(){},
+			onDrop : function(){}
+		}; 
 		
+		var options = $.extend({}, defaults, options); 
 		
-		function nester(element, options){
+		var $this,
+			children,
+			origin,
+			previous,
+			grabY,
+			grabX,
+			pageY,
+			pageX,
+			helper,
+			target,
+			offset,
+			height,
+			mouse;
+		
+		$this = $(this);
+		
+		children = $this.find('li');
+		children.dragg();
+		
+		$this.on('dragstart', 'li', function(e){
 			
-			var children,
-				origin,
-				previous,
-				grabY,
-				grabX,
-				pageY,
-				pageX,
-				helper,
-				target,
-				offset,
-				height,
-				mouse;
+			e.stopPropagation();
 			
-			children = $(element).find('li');
+			origin = $(this);
+			helper = $('#helper');
+			target = '';
+			grabY = pageY;
+			grabX = pageX;
+			mouse = "down";
 			
-			children.dragg();
-						
-			$(element).on('dragstart', 'li', function(e){
-								
+			origin.css('visibility', '');
+			origin.css('opacity', 0.5);
+			
+			origin.toggleClass('no-target').find('li').toggleClass('no-target');
+			
+			if($.isFunction(options.onStart)) {
+				options.onStart.call(this);
+			}
+			
+			$this.find('li').on('dragin', function(e){
 				e.stopPropagation();
-				
-				origin = $(this);
-				helper = $('#helper');
-				target = '';
-				grabY = pageY;
-				grabX = pageX;
-				mouse = "down";
-				
-				origin.toggleClass('no-target').find('li').toggleClass('no-target');
-				
-				$(element).find('li').on('dragin', function(e){
-					e.stopPropagation();
-					if(!$(this).hasClass('no-target')){
+				if(!$(this).hasClass('no-target')){
 						target = $(this);
 						offset = target.offset();
 						height = target.height();
 						bottom = offset.top+height;
-					}
-				});
+				}
+			});
 
-				$(element).find('li').on('dragout', function(e){
-					
+			$this.find('li').on('dragout', function(e){
+				
+			});
+		});
+		
+		$(document).on('drag', function(e){
+			if(target){
+				
+				var parents_limit = target.parents('li').length;
+				var children_limit = origin.find('ul li').length+parents_limit;
+				
+				console.log(children_limit);
+				
+				if(
+				(helper.offset().left>=(offset.left+10)) &&
+				(parents_limit<(options.maxLevels-1)) &&
+				(children_limit<(options.maxLevels-1))
+				)
+				{
+					if(target.children('ul').length==0){
+						target.append('<ul></ul>').children('ul').append(origin);
+					}
+				}
+				else {
+					if(pageY>(offset.top+(height/2))){
+						target.after(origin);
+					}
+					else if(pageY<bottom){
+						target.before(origin);
+					}
+					else target.after(origin);
+				}
+			}
+			if($.isFunction(options.onDrag)) {
+				options.onDrag.call(this);
+			}
+		});
+		
+		$(document).on('mousemove', $this, function(e){
+			pageY = e.pageY;
+			pageX = e.pageX;
+			if(mouse=='down'){
+				$this.find('li').children('ul').each(function(index, element) {
+					if($(this).children('li').length==0) $(this).remove();
 				});
+			}
+		});
+		
+		$(document).on('drop', $this, function(e){
+			mouse = 'up';
+			origin.toggleClass('no-target').find('li').toggleClass('no-target');
+			$(document).off('dragin, dragout');
+			origin.css('opacity', '');
+			if($.isFunction(options.onDrop)) {
+				options.onDrop.call(this);
+			}
+		});
+		
+	}
+	
+	
+	var methods = {
+		
+		init : function(options){
+			
+			return this.each(function(e){
+				
+				runNester.call(this, options);
+				
 			});
 			
-			
-			$(document).on('drag', function(e){
-				if(target){
-					if(helper.offset().left>=(offset.left+10)){
-						if(target.children('ul').length==0){
-							target.append('<ul></ul>').children('ul').append(origin);
-						}
-					}
-					else {
-						if(pageY>(offset.top+(height/2))){
-							target.after(origin);
-						}
-						else if(pageY<bottom){
-							target.before(origin);
-						}
-						else target.after(origin);
-					}
-				}
+		},
+		destroy: function(options) {
+			return $(this).each(function() {
+				var $this = $(this);
+				
+				
+				$($this).off('dragstart');
+				$($this).off('drag');
+				$(document).off('mousemove');
+				$(document).off('drop');
+				
+				$this.removeData('nester');
+				
 			});
+		},
+		toHierarchy: function(options) {
 			
+			var defaults = {
+				parser: function(){return $(this).index();}
+			}; 
 			
-			$(document).on('mousemove', function(e){
-				pageY = e.pageY;
-				pageX = e.pageX;
-				if(mouse=='down'){
-					origin.css('visibility', '');
-					origin.css('opacity', 0.5);
-					$(element).find('li').children('ul').each(function(index, element) {
-						if($(this).children('li').length==0) $(this).remove();
-                    });
-				}
-			});
+			var options = $.extend({}, defaults, options);
 			
+			return getHierarchy.call(this, options);
 			
-			$(document).on('drop', element, function(e){
-				mouse = 'up';
-				origin.toggleClass('no-target').find('li').toggleClass('no-target');
-				$(document).off('dragin, dragout');
-				origin.css('opacity', '');
-			});
+		},
+		toArray: function(options) {
 			
+			var defaults = {
+				parser: function(){return $(this).index();}
+			}; 
+			
+			var options = $.extend({}, defaults, options);
+			
+			return getArray.call(this, options);
+			
+		},
+		serialize: function(options) {
+			
+			var defaults = {
+				parser: function(){return $(this).index();}
+			}; 
+			
+			var options = $.extend({}, defaults, options);
+							
+			return getSerialized.call(this, options);
 			
 		}
-				
+		
+		
 	}
+	
+	var getHierarchy = function(options){
+		
+		var root = $(this).children('li');
+		var elements = jQuery.makeArray(root);
+		var finalArray = new Array();
+		
+		$(elements).each(function(index, element) {
+			
+			var subArray = {};
+			
+			if($.isFunction(options.parser)) {
+				options.parser.call(element);
+			}
+			
+			subArray.item = options.parser.call(this);
+			
+			var children = $(this).children('ul');
+			if(children.length>0) subArray.children = getHierarchy.call(children, options);
+			
+			finalArray.push(subArray);
+			
+        });
+		
+		return finalArray;
+		
+	}
+	
+	var getArray = function(options){
+		
+		var root = $(this).find('li');
+		var elements = jQuery.makeArray(root);
+		var finalArray = new Array();
+		
+		$(elements).each(function(index, element) {
+			
+			var subArray = {};
+			
+			if($.isFunction(options.parser)) {
+				options.parser.call(element);
+			}
+			
+			subArray.item = options.parser.call(element);
+			subArray.parent = options.parser.call($(element).parent('ul').parent('li')) || 'root';
+		
+			finalArray.push(subArray);
+			
+        });
+		
+		return finalArray;
+		
+	}
+	
+	var getSerialized = function(options){
+		
+		var root = $(this).find('li');
+		var elements = jQuery.makeArray(root);
+		var serialized = '';
+		
+		$(elements).each(function(index, element) {
+			
+			if(index>0) serialized +='&';
+			
+			if($.isFunction(options.parser)) {
+				options.parser.call(element);
+			}
+			
+			serialized += 'item['+options.parser.call(element)+']=';
+			serialized += options.parser.call($(element).parent('ul').parent('li')) || null;
+			
+        });
+		
+		return serialized;
+		
+	}
+	
+	$.fn.nester = function(method, options) {
+		
+		if(!method || jQuery.isPlainObject(method) || !methods[method]){
+			options = method;
+			method = 'init';
+		}
+		
+		return methods[method].call(this, options);
+		
+					
+	}
+	
+	
+	
+	
 	
 })( jQuery );
